c0e7c5ea8fd3559394aa40ffb5eb785872cbcb18
Carlos Carrillo <carlos.e119@gmail.com>
2019-05-15 16:57:23 -0400

    Forma app

---
+++ src/components/Cars/index.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React, { Component } from 'react';
+
+class Cars extends Component {
+  render() {
+    return (
+      <div className="Cars">
+        <h1>Cars Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Cars;
---
+++ src/data/menu.js
@@ -0,0 +1,15 @@
+export default [
+    {
+      title: 'Home',
+      url: '/'
+    },
+    {
+      title: 'About Us',
+      url: '/about'
+    },
+    {
+      title: 'Cars',
+      url: '/cars'
+    }
+  ];
+  
