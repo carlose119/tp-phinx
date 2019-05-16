316a1475ab1a2193a6f203be0b6728b46bcf5629
Carlos Carrillo <carlos.e119@gmail.com>
2019-05-16 16:39:42 -0400

    formulario manejado por pronps

--- src/components/Home/FormCars.js
+++ src/components/Home/FormCars.js
@@ -7,194 +7,60 @@ import { Form, Icon, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 class FormCars extends Component {
-    constructor () {
-        super();
-        this.state = {
-            uploadValue: 0,
-            picture: null,
-            marca: null,
-            ano: null,
-            origen: null,
-            velocidad: null,
-            estado: null,
-            file: null
-        };
-
-        this.handelUpload = this.handelUpload.bind(this); 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmitCars = this.handleSubmitCars.bind(this);
-    }
-
-    handelUpload (event) {
-        const file = event.target.files[0];
-        const storageRef = firebase.storage().ref(`fotos/${file.name}`);
-        const task = storageRef.put(file);
-
-        // Listener que se ocupa del estado de la carga del fichero
-        task.on('state_changed', snapshot => {
-            // Calculamos el porcentaje de tamaño transferido y actualizamos
-            // el estado del componente con el valor
-            let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            this.setState({
-                uploadValue: percentage
-            })
-        }, error => {
-            // Ocurre un error
-            console.error(error.message);
-        }, () => {
-            // Subida completada
-            // Obtenemos la URL del fichero almacenado en Firebase storage
-            // Obtenemos la referencia a nuestra base de datos 'pictures'
-            // Creamos un nuevo registro en ella
-            // Guardamos la URL del enlace en la DB
-            /*const record = {
-            photoURL: this.state.user.photoURL,
-            displayName: this.state.user.displayName,
-            image: task.snapshot.downloadURL
-            }
-            const dbRef = firebase.database().ref('pictures');
-            const newPicture = dbRef.push();
-            newPicture.set(record);*/
-
-            this.setState({
-                uploadValue: 100,
-                picture: task.snapshot.downloadURL
-            });
-        });
-    }
-
-    handleInputChange(e){
-        if(e.target.id === 'inputMarca') {
-          this.setState({
-            marca: e.target.value
-          });
-        }
     
-        if(e.target.id === 'inputAno') {
-          this.setState({
-            ano: e.target.value
-          });
-        }
-
-        if(e.target.id === 'inputOrigen') {
-            this.setState({
-                origen: e.target.value
-            });
-        }
-
-        if(e.target.id === 'inputVelocidad') {
-            this.setState({
-                velocidad: e.target.value
-            });
-        }
-
-        if(e.target.id === 'inputEstado') {
-            this.setState({
-                estado: e.target.value
-            });
-        }
-
-        if(e.target.id === 'inputFile') {
-            this.setState({
-                file: e.target.files[0]
-            });
-        }
-    }
-
-    handleSubmitCars (e) {
-        e.preventDefault();
-
-        const file = this.state.file;
-        const storageRef = firebase.storage().ref(`fotos/${file.name}`);
-        const task = storageRef.put(file);
-
-        // Listener que se ocupa del estado de la carga del fichero
-        task.on('state_changed', snapshot => {
-            // Calculamos el porcentaje de tamaño transferido y actualizamos
-            // el estado del componente con el valor
-            let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            this.setState({
-                uploadValue: percentage
-            })
-        }, error => {
-            // Ocurre un error
-            console.error(error.message);
-        }, () => {
-            // Subida completada
-            // Obtenemos la URL del fichero almacenado en Firebase storage
-            // Obtenemos la referencia a nuestra base de datos 'pictures'
-            // Creamos un nuevo registro en ella
-            // Guardamos la URL del enlace en la DB
-            /*const record = {
-            photoURL: this.state.user.photoURL,
-            displayName: this.state.user.displayName,
-            image: task.snapshot.downloadURL
-            }
-            const dbRef = firebase.database().ref('pictures');
-            const newPicture = dbRef.push();
-            newPicture.set(record);*/
-
-            this.setState({
-                uploadValue: 100,
-                picture: task.snapshot.downloadURL
-            });
-        });
-
-    }
-
     render () {
         return (
             <div>
-                <progress value={this.state.uploadValue} max='100'>
-                    {this.state.uploadValue} %
+                <progress value={this.props.uploadValue} max='100'>
+                    {this.props.uploadValue} %
                 </progress>
                 
-                <img width="320" src={this.state.picture} alt=""/>
+                <img width="320" src={this.props.picture} alt=""/>
 
-                <Form onSubmit={this.handleSubmitCars} className="login-form">
+                <Form onSubmit={this.props.onUpload} className="login-form">
                     <Form.Item>          
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             placeholder="Marca"
-                            value={this.state.marca}
+                            value={this.props.inputMarca}
                             id="inputMarca"
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />
                     </Form.Item>
                     <Form.Item>          
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             placeholder="Año de fabricación"
-                            value={this.state.ano}
+                            value={this.props.inputAno}
                             id="inputAno"
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />
                     </Form.Item>
                     <Form.Item>          
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             placeholder="Origen (Pais)"
-                            value={this.state.origen}
+                            value={this.props.inputOrigen}
                             id="inputOrigen"
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />
                     </Form.Item>
                     <Form.Item>          
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             placeholder="Velocidad máxima"
-                            value={this.state.velocidad}
+                            value={this.props.inputVelocidad}
                             id="inputVelocidad"
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />
                     </Form.Item>
                     <Form.Item>          
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                             placeholder="Estado"
-                            value={this.state.estado}
+                            value={this.props.inputEstado}
                             id="inputEstado"
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />
                     </Form.Item>
                     <Form.Item>          
@@ -204,7 +70,7 @@ class FormCars extends Component {
                             placeholder="file"
                             id="inputFile"
                             //onChange={this.handelUpload}
-                            onChange={this.handleInputChange}
+                            onChange={this.props.changeInput}
                         />          
                     </Form.Item>
                     <Form.Item>          
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -19,9 +19,20 @@ class Home extends Component {
       login: false,
       user: '',
       password: '',
+
+      uploadValue: 0,
+      picture: null,
+
+      marca: null,
+      ano: null,
+      origen: null,
+      velocidad: null,
+      estado: null,
+      file: null
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmitCars = this.handleSubmitCars.bind(this);
   }
 
   componentDidMount(){
@@ -44,6 +55,42 @@ class Home extends Component {
         password: e.target.value
       });
     }
+
+    if(e.target.id === 'inputMarca') {
+      this.setState({
+        marca: e.target.value
+      });
+    }
+
+    if(e.target.id === 'inputAno') {
+      this.setState({
+        ano: e.target.value
+      });
+    }
+
+    if(e.target.id === 'inputOrigen') {
+        this.setState({
+            origen: e.target.value
+        });
+    }
+
+    if(e.target.id === 'inputVelocidad') {
+        this.setState({
+            velocidad: e.target.value
+        });
+    }
+
+    if(e.target.id === 'inputEstado') {
+        this.setState({
+            estado: e.target.value
+        });
+    }
+
+    if(e.target.id === 'inputFile') {
+        this.setState({
+            file: e.target.files[0]
+        });
+    }
   }
 
   handleSubmitLogin = e => {
@@ -62,6 +109,47 @@ class Home extends Component {
     }
     
   };
+
+  handleSubmitCars (e) {
+    e.preventDefault();
+
+    const file = this.state.file;
+    const storageRef = firebase.storage().ref(`fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    // Listener que se ocupa del estado de la carga del fichero
+    task.on('state_changed', snapshot => {
+        // Calculamos el porcentaje de tamaño transferido y actualizamos
+        // el estado del componente con el valor
+        let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({
+            uploadValue: percentage
+        })
+    }, error => {
+        // Ocurre un error
+        console.error(error.message);
+    }, () => {
+        // Subida completada
+        // Obtenemos la URL del fichero almacenado en Firebase storage
+        // Obtenemos la referencia a nuestra base de datos 'pictures'
+        // Creamos un nuevo registro en ella
+        // Guardamos la URL del enlace en la DB
+        /*const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+        }
+        const dbRef = firebase.database().ref('pictures');
+        const newPicture = dbRef.push();
+        newPicture.set(record);*/
+
+        this.setState({
+            uploadValue: 100,
+            picture: task.snapshot.downloadURL
+        });
+    });
+
+  }
 
   renderHomeContent () {
     if(!this.state.login) {
@@ -97,7 +185,19 @@ class Home extends Component {
       return (
         <div>
           <h1>Login</h1>
-          <FormCars />
+          <FormCars 
+            onUpload={ this.handleSubmitCars } 
+            changeInput={this.handleInputChange}
+
+            uploadValue={this.state.uploadValue}
+            picture={this.state.picture}
+
+            inputMarca={this.state.marca}
+            inputAno={this.state.ano}
+            inputOrigen={this.state.origen}
+            inputVelocidad={this.state.velocidad}
+            inputEstado={this.state.estado}
+          />
         </div>
       );
     }
